c5fbf1498ccaf86cf3ec8b82a66eb2bf43abb84f
nuvia <nuvia@LAPTOP-U5Q2MDPO>
2021-11-23 22:41:43 -0500

    First time checking

---
+++ automationtraining/src/main/java/peoplentech/automationtraining/App.java
@@ -0,0 +1,13 @@
+package peoplentech.automationtraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
